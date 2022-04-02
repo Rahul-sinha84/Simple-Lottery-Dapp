0d150e9b9909e71ab2a74f4086edea06b2378b37
Ubuntu <rahulchampion48@gmail.com>
2022-04-02 17:51:36 +0530

    refactor done

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import { useState, useEffect } from "react";
-import { ethers } from "ethers";
-import Artifacts from "../artifacts/contracts/Lottery.sol/Lottery.json";
-const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+import {
+  checkMetamaskStatus,
+  connectMetamask,
+  firstFunc,
+  listenToEvents,
+  contractAddress
+} from "../components/configureMetamask";
 
 const Main = () => {
-  const [signer, setSigner] = useState(null);
   const [metamaskConnected, setMetamaskConnected] = useState(false);
   const [contractInfo, setContractInfo] = useState({
     minAmount: null,
@@ -20,78 +23,25 @@ const Main = () => {
 
   //default
   useEffect(() => {
-    firstFunc();
-    checkMetamaskStatus();
+    firstFunc(
+      setContract,
+      setCurrentAccount,
+      setCurrentNetworkId,
+      setMetamaskConnected
+    );
+    checkMetamaskStatus(
+      setMetamaskConnected,
+      setCurrentAccount,
+      setCurrentNetworkId
+    );
   }, []);
 
   // for updating contract info-
   useEffect(() => {
-    getContractData(signer);
-    listenToEvents();
-  }, [signer, currentAccount, contract, load]);
-
-  const firstFunc = async () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const _signer = await provider.getSigner();
-    const _currentNetworkId = window.ethereum.networkVersion;
-    setCurrentNetworkId(_currentNetworkId);
-    setSigner(_signer);
-    const accounts = await provider.listAccounts();
-    if (accounts.length > 0) {
-      setCurrentAccount(accounts[0]);
-      setMetamaskConnected(true);
-    } else {
-      setMetamaskConnected(false);
-    }
-    await initialiseContract(_signer);
-  };
-
-  const checkMetamaskStatus = () => {
-    const accountChanged = (accounts) => {
-      setCurrentAccount(accounts[0]);
-      console.log(accounts[0], "account changed");
-      if (!accounts.length) {
-        setMetamaskConnected(false);
-      }
-    };
-    const disconnectAccount = () => {
-      setCurrentAccount("");
-      console.log("disconnected account");
-      setMetamaskConnected(false);
-    };
-    const connected = async () => {
-      const changedAccounts = await window.ethereum.request({
-        method: "eth_requestAccounts",
-      });
-      setCurrentAccount(changedAccounts[0]);
-      console.log("connected");
-      setMetamaskConnected(true);
-    };
-    const chainChainged = (chain) => {
-      const _currentNetworkId = parseInt(chain, 16);
-      setCurrentNetworkId(_currentNetworkId);
-    };
-    window.ethereum.on("disconnect", disconnectAccount);
-    window.ethereum.on("accountsChanged", accountChanged);
-    window.ethereum.on("connect", connected);
-    window.ethereum.on("chainChanged", chainChainged);
-    return () => {
-      window.ethereum.off("disconnect", disconnectAccount);
-      window.ethereum.off("accountsChanged", accountChanged);
-      window.ethereum.off("connect", connected);
-      window.ethereum.off("chainChanged", chainChainged);
-    };
-  };
-
-  const connectMetamask = async () => {
-    await window.ethereum
-      .request({ method: "eth_requestAccounts" })
-      .then((_) => setMetamaskConnected(true))
-      .catch((err) => {
-        console.log(err);
-        setMetamaskConnected(false);
-      });
-  };
+    getContractData();
+    // for listening of events
+    // listenToEvents(contract);
+  }, [currentAccount, contract, load]);
 
   const getContractData = async () => {
     if (contract) {
@@ -107,16 +57,6 @@ const Main = () => {
       _amountDonated = parseInt(_amountDonated._hex, 16);
       setAmountDonated(_amountDonated);
     }
-  };
-
-  const initialiseContract = async (_signer) => {
-    const _contract = new ethers.Contract(
-      contractAddress,
-      Artifacts.abi,
-      _signer
-    );
-    setContract(_contract);
-    getContractData();
   };
 
   const handleDonate = async () => {
@@ -145,31 +85,11 @@ const Main = () => {
     setLoad(!load);
   };
 
-  const listenToEvents = async () => {
-    const winnerDeclaration = (winner, amount, msg) => {
-      alert(
-        `${winner} is the winner, ${amount} has been send to their account !!`
-      );
-    };
-
-    const donatedAmount = (from, amount, msg) => {
-      alert(`${from} donated ${amount} !!`);
-    };
-
-    if (contract) {
-      contract.on("WinnerDeclaration", winnerDeclaration);
-      contract.on("DonationSuccessful", donatedAmount);
-
-      return () => {
-        contract.off("WinnerDeclaration", winnerDeclaration);
-        contract.off("DonationSuccessful", donatedAmount);
-      };
-    }
-  };
-
   return (
     <div>
-      <button onClick={connectMetamask}>Connect Metamask</button>
+      <button onClick={() => connectMetamask(setMetamaskConnected)}>
+        Connect Metamask
+      </button>
       {`${metamaskConnected}`}
       {currentAccount}
       <br />
